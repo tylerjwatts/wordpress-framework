cc82af1c36c6b338e62f03eae92316095632ba92
Tyler Watts <tylerjoewatts@gmail.com>
2021-11-18 15:21:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# WordPress Starter Theme with Webpack Workflow
+
+# Installation
+
+###### Open terminal/cmd:
+
+**Generate WordPress files, MySQL Installation, and PHPMyAdmin**
+`docker-compose up -d`
+
+###### Local installation details:
+
+**WordPress Installation:**
+http://localhost:8000
+
+**PHPMyAdmin**
+http://localhost:8080
+`DB User: wordpress`
+`DB Password: wordpress`
+
+###### Open terminal/cmd:
+
+**Navigate to theme directory**
+
+`npm install`
+
+`npm run dev`
+`npm run dev:watch`
+
+`npm run prod`
+`npm run prod:watch`
---
+++ wp-content/themes/starter/footer.php
@@ -0,0 +1,22 @@
+
+        </main>
+        <!-- Main End -->
+
+        <!-- Footer Start -->
+        <footer class="footer">
+            <div class="footer__wrap wrap">
+                <div class="footer__content">
+                    <p class="footer__copyright">&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>.</p>
+                </div>
+            </div>
+        </footer>
+        <!-- Footer End -->
+
+        <!-- WordPress Footer Scripts Start -->
+        <section id="wp-footer">
+            <?php wp_footer(); ?>
+        </section>
+        <!-- WordPress Footer Scripts End -->
+
+    </body>
+</html>
